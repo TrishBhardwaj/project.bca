585b5374a6f4d292c7e53a68f9f6bb5663aa29a0
TrishBhardwaj <trishbhardwaj856@gmail.com>
2024-10-23 23:40:05 +0530

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+}
+
+// Optional: Log a welcome message to the console
+console.log("Welcome to Furniture Co.!");
